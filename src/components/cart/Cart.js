3ffcb83bd6e6fa4e87a952b467fdf998d5658945
hatanvir5 <hatanvir5@gmail.com>
2021-09-26 23:44:56 +0600

    fainal-commit

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,15 +3,16 @@ import React from 'react';
 const cart = (props) => {
     const { cart } = props;
     let totalCost = 0;
+    let name=[];
     for (const teacher of cart) {
        totalCost=teacher.Salary + totalCost;
+       name=teacher.name
         }
-        console.log(cart);
     return (
         <div>
         <h2>Teacher Added:{cart.length}</h2>
         <h4>Total Cost: ${totalCost}</h4>
-        <h3>{}</h3>
+        <h3>{name}</h3>
     </div>
     );
 };
